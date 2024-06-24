37c665e454fc3d3d8b2f0c76616f6b8c8f004b78
praisymiracline11 <praisymircline12@gmail.com>
2024-06-24 14:59:39 +0530

    Add python.py:Initial commit

--- python.py
+++ python.py
@@ -2,3 +2,17 @@ stu_name="praisy"
 age=20
 print(stu_name,"is",age)
 
+name,age,city="praisy",20,"chennai"
+print(name)
+print(age)
+print(city)
+
+name=str(name)
+age=int(age)
+
+num1=20
+num2=11
+if num1>num2:
+ print("num1 is greater")
+if num2<num1:
+ print("num2 is smaller")
